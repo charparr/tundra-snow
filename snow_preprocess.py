5a9e24753114df31255e7a2d3c31508002d5ea32
Charlie Parr <cparr4@alaska.edu>
2016-03-24 14:56:25 -0800

    updated function
    including drift and scour masks, drift and scour normal snow, drift and scour real snow. changed 'arr_tif' to 'real_snow' for clarity

--- snow_preprocess.py
+++ snow_preprocess.py
@@ -23,24 +23,30 @@ src_dict = {}
 # Can use glob to pull all snow rasters in a certain folder
 test = ['/home/cparr/water_tracks/sub_wt_2012.tif', '/home/cparr/water_tracks/sub_wt_2013.tif']
 
-def read_gtiffs(tif_list):
+def read_gtiffs( tif_list ):
     
     for f in tif_list:
         
         src_name = os.path.basename( f ).replace( '.tif', '_src' )
-        src = rasterio.open(f)
+        src = rasterio.open( f )
         meta = src.meta
         
-        arr_tif = np.ma.masked_values( src.read(1), src.nodata )
-        blur_snow = cv2.GaussianBlur(arr_tif, (3,3), 0)
-        norm_snow = maxabs_scale(blur_snow, axis=1, copy=True)
+        real_snow = np.ma.masked_values( src.read(1), src.nodata )
+        blur_snow = cv2.GaussianBlur( real_snow, (3,3), 0 )
+        norm_snow = maxabs_scale( blur_snow, axis=1, copy=True )
+        drifted_mask = np.ma.MaskedArray( norm_snow > 0.5 )
+        scoured_mask = np.ma.MaskedArray( norm_snow <= 0.5 )
+        drifted_norm_snow = drifted_mask*norm_snow
+        drifted_real_snow = drifted_mask*real_snow
+        scoured_norm_snow = scoured_mask*norm_snow
+        scoured_real_snow = scoured_mask*real_snow
+        
 
         
-        src_dict[src_name] = arr_tif, meta, blur_snow, norm_snow
+        src_dict[src_name] = real_snow, meta, blur_snow, norm_snow,drifted_mask, scoured_mask,drifted_norm_snow, drifted_real_snow,scoured_norm_snow, scoured_real_snow
         #return src_dict
         
-    
-read_gtiffs(test)
+read_gtiffs( test )
 
 i = 1
 for k,v in src_dict.iteritems():
